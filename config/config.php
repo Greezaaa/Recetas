3cd420911c5a44c8ba628709231c33cb2ac0d87e
Jeka <greezaaa@gmail.com>
2022-03-31 00:53:25 +0200

    config fix

--- config/config.php
+++ config/config.php
@@ -1,14 +1,12 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-$env = getenv();
-$dbserver = $env['DB_SERVER'] ?: 'localhost';
-$dbname = $env['DB_NAME'] ?: 'blog_recetas';
-$user = $env['DB_USER'] ?: 'root';
-$pwd = $env['DB_PASSWORD'] ?: '';
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'blog_recetas');
 /* Attempt to connect to MySQL database */
 try {
-    $pdo = new PDO("mysql:host=" . $dbserver . ";dbname=" . $dbname, $user, $pwd);
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+
     // cambiamos idioma de datos al resibir desde la base de datos
     $acentos = $pdo->query("SET NAMES 'utf8'");
     // Set the PDO error mode to exception
@@ -16,3 +14,19 @@ try {
 } catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
 }
+
+// $env = getenv();
+// $dbserver = $env['DB_SERVER'] ?: 'localhost';
+// $dbname = $env['DB_NAME'] ?: 'blog_recetas';
+// $user = $env['DB_USER'] ?: 'root';
+// $pwd = $env['DB_PASSWORD'] ?: '';
+// /* Attempt to connect to MySQL database */
+// try {
+//     $pdo = new PDO("mysql:host=" . $dbserver . ";dbname=" . $dbname, $user, $pwd);
+//     // cambiamos idioma de datos al resibir desde la base de datos
+//     $acentos = $pdo->query("SET NAMES 'utf8'");
+//     // Set the PDO error mode to exception
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("ERROR: Could not connect. " . $e->getMessage());
+// }
